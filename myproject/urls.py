80b7e22bddbb50d60df7e9db26bebb64965d4ac5
Albert Otoo <95134630+Albertjr009@users.noreply.github.com>
2026-07-21 12:43:45 +0000

    got the dynamic urls and board topics view working

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,7 +4,7 @@ from boards import views
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
-    
-
+    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 ]
