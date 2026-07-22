ea8cfcae2bcd4f863648eac75464ff9273df36f4
Albert Otoo <95134630+Albertjr009@users.noreply.github.com>
2026-07-22 23:42:15 +0000

    fixed url layout typos and upgraded dropdown to bootstrap 5 bundle

---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def manual_logout_view(request):
+    django_logout(request)
+    return redirect('home')
